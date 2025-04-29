bfad7c51210e8d9ff729327b01f0e3d02fe8d756
z-for-zarrin <156834963+z-for-zarrin@users.noreply.github.com>
2025-04-29 16:48:18 +0100

    Revert "Patch/file separation"

--- logic/components/player.py
+++
@@ -1,6 +0,0 @@
-class Player:
-    def __init__(self, id, player_name, colour, powerups):
-        self.id = id
-        self.player_name = player_name
-        self.colour = colour
-        self.powerups = powerups
